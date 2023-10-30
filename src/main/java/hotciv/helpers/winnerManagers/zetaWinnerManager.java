339f337445fde03ea14ad3ba76cfba61d34f7123
TRE26 <tre26@pitt.edu>
2023-10-30 17:11:39 -0400

    refactored zetaWinnerManager

--- src/main/java/hotciv/helpers/winnerManagers/zetaWinnerManager.java
+++ src/main/java/hotciv/helpers/winnerManagers/zetaWinnerManager.java
@@ -5,27 +5,19 @@ import hotciv.framework.Position;
 import hotciv.helper_Interfaces.winnerManager;
 import hotciv.standard.GameImpl;
 
-public class zetaWinnerManager implements winnerManager{
+public class zetaWinnerManager implements winnerManager {
+
     @Override
     public Player getWinner(GameImpl game) {
+        winnerManager subStrategy;
+
         if (game.getRoundNumber() <= 20) {
-            Position city1 = new Position(1, 1);
-            Position city2 = new Position(1, 4);
-            if ((game.getCityAt(city1).getOwner() == game.getCityAt(city2).getOwner())) {
-                return game.getCityAt(city1).getOwner();
-            }
-            return null;
+            subStrategy = new betaWinnerManager();
+        } else {
+            subStrategy = new epsilonWinnerManager();
         }
 
-        else{
-            int[] numAttacks = game.getNumberOfSuccessfulAttacks();
-            for (int i = 0; i < game.getNumberOfPlayers(); i++){
-                if (numAttacks[i] >= 3){
-                    return Player.values()[i];
-                }
-            }
-        }
-        return null;
+        return subStrategy.getWinner(game);
     }
 }
 
