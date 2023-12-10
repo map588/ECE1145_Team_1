1a4d14e6a5355f57c1227cfad6f588b4c5448772
map588 <map588@pitt.edu>
2023-12-09 21:16:04 -0500

    started full_game

---
+++ src/main/java/hotciv/standard/full_game.java
@@ -0,0 +1,12 @@
+package hotciv.standard;
+
+import hotciv.framework;
+import hotciv.helpers;
+import hotciv.helper_Interfaces;
+import hotciv.manager_factories;
+import hotciv.object_factories.*;
+
+public class full_game {
+    GameImpl game = new GameImpl(new AlphaFactory());
+
+}
